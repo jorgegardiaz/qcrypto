08258a373fca339465e324672b0d266777d088b6
jorgegardiaz <jorgegardiaz@gmail.com>
2026-05-14 11:03:47 +0100

    Create qia submodule under protocols

--- src/protocols.rs
+++ src/protocols.rs
@@ -1,8 +1,9 @@
 //! Quantum Cryptography Protocols.
 //!
 //! This module contains implementations of various quantum cryptography protocols,
-//! including QKD (Quantum Key Distribution) and authentication protocols.
+//! including QKD (Quantum Key Distribution) and QIA (Quantum Authentication Protocols).
 
-pub mod qia_qzkp;
+pub mod qia;
 pub mod qkd;
-pub use qkd::{b92, bb84, bbm92};
+pub use qia::qia_qzkp;
+pub use qkd::{b92, bb84, bbm92, e91, sarg04, six_state};
---
+++ src/protocols/qia.rs
@@ -0,0 +1,6 @@
+//! Quantum Identity Authentication (QIA) Protocols.
+//!
+//! This module contains implementations of the following QIA protocols:
+//! - **qia_qzkp**: QIA protocol involving a Quantum Zero Knowledge Proof (QZKP).
+
+pub mod qia_qzkp;
